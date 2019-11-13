e6e39fe7182a54249f33d0de89d806e734faf6fa
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-13 11:40:06 -0600

    Update

--- generate_image_3D_make_scattering.py
+++ generate_image_3D_make_scattering.py
@@ -1,40 +1,25 @@
 # restore training set
 import numpy as np
-y = np.load('sparse_grid_final_512pix_y_test.npy')
 
-choose = [np.where((y[:,0] > 0.7) == 1)[0],
-          np.where((np.abs(y[:,0]-0.6) < 0.05)*(np.abs(y[:,1]-0.4) < 0.05) == 1)[0],
-          np.where((np.abs(y[:,0]-0.57) < 0.05)*(np.abs(y[:,1]-0.5) < 0.05))[0],
-          np.where((np.abs(y[:,0]-0.5) < 0.05)*(np.abs(y[:,1]-0.6) < 0.05))[0],
-          np.where((np.abs(y[:,0]-0.45) < 0.03)*(np.abs(y[:,1]-0.7) < 0.05))[0],
-          np.where((np.abs(y[:,0]-0.4) < 0.03)*(np.abs(y[:,1]-0.75) < 0.05))[0],
-          np.where((np.abs(y[:,0]-0.37) < 0.03)*(np.abs(y[:,1]-0.8) < 0.05))[0],
-          np.where((np.abs(y[:,0]-0.3) < 0.01)*(np.abs(y[:,1]-0.8) < 0.02))[0],
-          np.where((np.abs(y[:,0]-0.3) < 0.01)*(np.abs(y[:,1]-0.84) < 0.02))[0],
-          np.where((np.abs(y[:,0]-0.25) < 0.02)*(np.abs(y[:,1]-0.9) < 0.03))[0],
-          np.where((np.abs(y[:,0]-0.22) < 0.02)*(np.abs(y[:,1]-1.0) < 0.03))[0],
-          np.where((y[:,0] < 0.21)*(y[:,1] > 1.05))[0]]
+temp = np.load('../Zeldovich_Approximation.npz')
+sim_z0 = temp["sim_z0"]
+sim_z50 = temp["sim_z50"]
 
-
+#-------------------------------------------------------------------------------------
 # import packages
-from kymatio import Scattering2D
+from kymatio import HarmonicScattering3D
 
 # make scattering coefficients
 J_choice = 5
-L_choice = 1
+L_choice = 4
 max_order_choice = 1
-scattering = Scattering2D(J=J_choice, shape=(512,512),\
+scattering = HarmonicScattering3D(J=J_choice, shape=(64,64,64),\
                           L=L_choice, max_order=max_order_choice)
-scattering.cuda()
+#scattering.cuda()
 
 import torch
-scatter_coeff = []
-for i in range(len(choose)):
-    print(i)
-    x_image = np.load('../weak_lensing_data/sparse_grid_final_512pix_x_test.npy')[choose[i],:,:,0]
-    x_image = torch.from_numpy(x_image).type(torch.cuda.FloatTensor) + 5.
-    scatter_coeff.append(np.median(scattering(x_image).mean(dim=(2,3)).log().cpu().detach().numpy(), axis=0))
-scatter_coeff = np.array(scatter_coeff)
+x_image = torch.from_numpy(sim_z0).type(torch.cuda.FloatTensor)
+scatter_coeff = scattering(x_image).mean(dim=(2,3,4)).cpu().detach().numpy()
 
 # save results
-np.save("scatter_coeff_max_order=1.npy", scatter_coeff)
+np.save("../scatter_coeff_max_order=" + str(max_order_choice) + ".npy", scatter_coeff)
