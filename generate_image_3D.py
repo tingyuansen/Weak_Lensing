7e1109a1cc2bd1186cba2184bf2296463b0c6a6a
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-13 19:04:56 -0600

    Update

--- generate_image_3D.py
+++ generate_image_3D.py
@@ -78,7 +78,7 @@ def main():
         # optimize
         for i in range(int(num_step)):
             scattering_coeff = scattering(model_fit.param).view(-1).log();
-            loss = ((target_coeff-scattering_coeff).abs()).sum();
+            loss = ((target_coeff[1:]-scattering_coeff[1:]).abs()).sum();
 
             #loss_2 = ((torch.sort(model_fit.param).values[0,::4] - CDF_t)**2).sum()/5.
             #print(loss_1/loss_2) # making sure the two losses are of the same order
