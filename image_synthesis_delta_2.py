da340b03cd0ef58207785abdb321fb4005841c71
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-05-19 18:30:14 -0400

    Update

--- image_synthesis_delta_2.py
+++ image_synthesis_delta_2.py
@@ -13,39 +13,111 @@ import numpy as np
 
 
 #=========================================================================================================
-# setup scattering
-# number of pixels
-num_pixel = 256
+def get_power_spectrum(target, bins):
+    '''
+    get the power spectrum of a given image
+    '''
+    M, N = target.shape
+    modulus = torch.fft(torch.cat((target.reshape(M,N,1), torch.zeros((M,N,1)).type(torch.cuda.FloatTensor) ), 2), 2)
+    modulus = (modulus[:,:,0]**2 + modulus[:,:,1]**2)**0.5
+    modulus = torch.cat(
+        ( torch.cat(( modulus[M//2:, M//2:], modulus[M//2:, :M//2] ), 0),
+          torch.cat(( modulus[:M//2, M//2:], modulus[:M//2, :M//2] ), 0)
+        ),1)
+    X = np.arange(0,M)
+    Y = np.arange(0,N)
+    Xgrid, Ygrid = np.meshgrid(X,Y)
+    R = ((Xgrid-M/2)**2+(Ygrid-N/2)**2)**0.5
+    R = torch.from_numpy(R).type(torch.cuda.FloatTensor)
+    R_range = torch.logspace(0.0, np.log10(M/2), bins).type(torch.cuda.FloatTensor)
+    R_range = torch.cat((torch.tensor([0]).type(torch.cuda.FloatTensor), R_range))
+    power_spectrum = torch.zeros(len(R_range)-1).type(torch.cuda.FloatTensor)
+    for i in range(len(R_range)-1):
+        select = (R >= R_range[i]) * (R<R_range[i+1])
+        power_spectrum[i] = modulus[select].mean()
+    return power_spectrum, R_range
 
-# define scattering
-J_choice = 5
-L_choice = 4
-max_order_choice = 2
-scattering = Scattering2D(J=J_choice, shape=(num_pixel,num_pixel),\
-                          L=L_choice, max_order=max_order_choice)
-scattering.cuda()
+def get_random_data(target, M, N, mode='image'):
+    '''
+    get a gaussian random field with the same power spectrum as the image 'target' (in the 'image' mode),
+    or with an assigned power spectrum function 'target' (in the 'func' mode).
+    '''
+
+    if mode == 'func':
+        random_phase = np.random.normal(0,1,(M//2-1,N-1)) + np.random.normal(0,1,(M//2-1,N-1))*1j
+        random_phase_left = (np.random.normal(0,1,(M//2-1)) + np.random.normal(0,1,(M//2-1))*1j)[:,None]
+        random_phase_top = (np.random.normal(0,1,(N//2-1)) + np.random.normal(0,1,(N//2-1))*1j)[None,:]
+        random_phase_middle = (np.random.normal(0,1,(N//2-1)) + np.random.normal(0,1,(N//2-1))*1j)[None,:]
+        random_phase_corners = np.random.normal(0,1,3)
+    if mode == 'image':
+        random_phase = np.random.rand(M//2-1,N-1)
+        random_phase_left = np.random.rand(M//2-1)[:,None]
+        random_phase_top = np.random.rand(N//2-1)[None,:]
+        random_phase_middle = np.random.rand(N//2-1)[None,:]
+        random_phase_corners = np.random.randint(0,2,3)/2
+    gaussian_phase = np.concatenate((
+                      np.concatenate((random_phase_corners[1][None,None],
+                                      random_phase_left,
+                                      random_phase_corners[2][None,None],
+                                      -random_phase_left[::-1,:],
+                                    ),axis=0),
+                      np.concatenate((np.concatenate((random_phase_top,
+                                                      random_phase_corners[0][None,None],
+                                                      -random_phase_top[:,::-1],
+                                                    ),axis=1),
+                                      random_phase,
+                                      np.concatenate((random_phase_middle,
+                                                      np.array(0)[None,None],
+                                                      -random_phase_middle[:,::-1],
+                                                    ),axis=1),
+                                      -random_phase[::-1,::-1],
+                                    ),axis=0),
+                                    ),axis=1)
+
+
+    if mode == 'image':
+        gaussian_modulus = np.abs(np.fft.fftshift(np.fft.fft2(target)))
+        gaussian_field = np.fft.ifft2(np.fft.fftshift(gaussian_modulus*np.exp(1j*2*np.pi*gaussian_phase)))
+    if mode == 'func':
+        X = np.arange(0,M)
+        Y = np.arange(0,N)
+        Xgrid, Ygrid = np.meshgrid(X,Y)
+        R = ((Xgrid-M/2)**2+(Ygrid-N/2)**2)**0.5
+        gaussian_modulus = target(R)
+        gaussian_modulus[M//2, N//2] = 0
+        gaussian_field = np.fft.ifft2(np.fft.fftshift(gaussian_modulus*gaussian_phase))
+
+    data = np.fft.fftshift(np.real(gaussian_field))
+    return data
 
 
 #=========================================================================================================
 # main body of the script
-def generate_image():
+def image_synthesis(image,
+                    learnable_param_list = [(100, 1e-3)],
+                    savedir = 'drive/My Drive/Colab Notebooks/ST Yuan-Sen/',):
 
-    # load an initial guess
-    image = np.load("poisson_process_image.npy")[0:1,:,:]
-    #CDF_t = torch.from_numpy(np.sort(image.flatten())).type(torch.cuda.FloatTensor) + 5.
+    image = image[None, :, :]
 
-#----------------------------------------------------------------------------------------------------------
-    # target ccoefficients
-    image_GPU = torch.from_numpy(image).type(torch.cuda.FloatTensor)
-    #print(image_initial.shape)
-    # scattering_target = Scattering2D(J=J_choice, shape=(256,256),\
-    #                               L=L_choice, max_order=max_order_choice)
-    # scattering_target.cuda()
-    # target_coeff = scattering_target(image_initial).mean(dim=(2,3))[0,:].log();
+    torch.manual_seed(986)
+    np.random.seed(986)
 
-    # L2 norm
-    #target_coeff = ((image_initial).abs()).mean()
-    #print(target_coeff)
+    # number of pixels
+    num_pixel = 256
+    J = 3
+    L = 4
+
+    # define scattering
+    # scattering = Scattering2D(J=J, shape=(num_pixel,num_pixel), L=L, max_order=2)
+    # scattering.cuda()
+
+    # Using cumulative distribution function to set PDF constraint
+    target_CDF = torch.from_numpy(np.sort(image.flatten())).type(torch.cuda.FloatTensor) + 5
+
+    image_GPU = torch.from_numpy(image).type(torch.cuda.FloatTensor) + 5
+    target_PS, _ = get_power_spectrum(image_GPU[0], 20)
+    target_PS = target_PS.log()
+    # target_ST = scattering(image_GPU).mean(dim=(2,3))[0,:].log();
 
 #----------------------------------------------------------------------------------------------------------
     # define mock image
@@ -53,30 +125,42 @@ def generate_image():
         def __init__(self):
             super(model_image, self).__init__()
 
-            # star with the same image but with random phase
-            self.param = torch.nn.Parameter(torch.rand(1,256,256).type(torch.cuda.FloatTensor))
+            # initialize with GRF of same PS as target image
+            if True:
+                self.param = torch.nn.Parameter(
+                    torch.from_numpy(
+                        get_random_data(image, num_pixel,num_pixel).reshape(1,-1)
+                    ).type(torch.cuda.FloatTensor) + 5
+                )
 
-            # self.param = torch.nn.Parameter(
-            #     torch.from_numpy(
-            #         np.random.uniform(size=(1,256,256))
-            #     ).type(torch.cuda.FloatTensor)
-            # )
+            # initialize with white noise GRF
+            if True:
+                self.param = torch.nn.Parameter(torch.randn(1,num_pixel,num_pixel).type(torch.cuda.FloatTensor)*0.2+6)
 
+            # initialize with smoothed target image
+            if False:
+                self.param = torch.nn.Parameter(
+                    torch.from_numpy(
+                        gaussian_filter(
+                            np.random.randn(1,num_pixel,num_pixel),
+                            (0,0.5,0.5)
+                            )
+                        ).type(torch.cuda.FloatTensor)*0.05 + 5
+                    )
+            # initialized with particular external image
+            if False:
+                train_result = np.load(savedir+'trained_result.npy')
+                self.param = torch.nn.Parameter(torch.from_numpy(train_result.reshape(1,-1)).type(torch.cuda.FloatTensor))
+
+            # others
+            if False:
+                pass
 #---------------------------------------------------------------------------------------------------------
-    # learn with different training rate
+
     model_fit = model_image()
-    learnable_param_list = [[100*50, 1e-3], [100*0, 1e-3], [100*0, 1e-4]]
 
-    # loop over training rate
+    # define learnable
     for learnable_group in range(len(learnable_param_list)):
-
-        # define learn hyper parameter
-        # num_step = learnable_param_list[learnable_group][0]
-        # learning_rate = learnable_param_list[learnable_group][1]
-        #
-        # # define optimizer
-        # optimizer = optim.SGD(model_fit.parameters(), lr=learning_rate)
-
         num_step = learnable_param_list[learnable_group][0]
         learning_rate = learnable_param_list[learnable_group][1]
 
@@ -85,24 +169,19 @@ def generate_image():
 
         # optimize
         for i in range(int(num_step)):
-            #scattering_coeff = scattering(model_fit.param.reshape(1,num_pixel,num_pixel))\
-            #                        .mean(dim=(2,3))[0,:].log();
-            #loss = ((target_coeff[1:]-scattering_coeff[1:])**2).sum(); # ignore the zeroth order (normalization)
-            #loss_2 = ((torch.sort(model_fit.param).values[0,::4] - CDF_t)**2).sum()/5.
-            #print(loss_1/loss_2) # making sure the two losses are of the same order
 
-            # loss: L2
-            # loss = ((((model_fit.param.reshape(1,num_pixel,num_pixel))**2).mean()**0.5 - \
-            #            ((image_initial)**2).mean()**0.5) / ((image_initial)**2).mean()**0.5 )**2
+            # loss: power spectrum
+            # PS, _ = get_power_spectrum(model_fit.param.reshape(num_pixel,num_pixel), 20)
+            # PS = PS.log()
+            # loss_PS = ((target_PS[:] - PS[:])**2).sum()
 
-            # loss: L1
-            #loss_L1 = (( (model_fit.param.reshape(1,num_pixel,num_pixel) - 5).abs().mean() - (image_GPU-5).abs().mean() )\
-            #            /(image_GPU-5).abs().mean() )**2
+            # loss: scattering_coeff
+            # ST = scattering(model_fit.param.reshape(1,num_pixel,num_pixel)).mean(dim=(2,3))[0,:].log();
+            # loss_ST = ((target_ST - ST)**2).sum();
 
-
-            # scattering_coeff = ((model_fit.param).abs()).mean();
-            # print(scattering_coeff)
-            # loss = ((target_coeff-scattering_coeff).abs()).sum()
+            # loss: CDF
+            # CDF = torch.sort(torch.flatten(model_fit.param.reshape(1,num_pixel,num_pixel))).values
+            # loss_CDF = ((target_CDF - CDF)**2).sum()
 
             # loss: L2
             loss_L2 = ((((model_fit.param.reshape(1,num_pixel,num_pixel) - 5)**2).mean()**0.5 - \
@@ -121,19 +200,47 @@ def generate_image():
             # loss: 1st moment
             # loss = loss_PS * 0 + loss_CDF * 0 + loss_bound * 0 +\
             #         loss_L1 + loss_L2 + loss_mean
-            loss = loss_L1 + loss_L2 + loss_mean
+            loss = loss_L1 + loss_L2*0 #+ loss_mean
 
-#---------------------------------------------------------------------------------------------------------
-            if i%50== 0:
-                print(i, loss)
-                #print((target_coeff[1:]-scattering_coeff[1:]).abs()/target_coeff[1:].abs())
+            if i%100== 0:
+                # save map
+                np.save(savedir + 'synthesis_results_step=' + str(i) + '.npy',
+                        model_fit.param.reshape(1,num_pixel,num_pixel).cpu().detach().numpy()-5);
+                np.save(savedir +'synthesis_results_final.npy',
+                        model_fit.param.reshape(1,num_pixel,num_pixel).cpu().detach().numpy()-5);
+                print(i)
+                print('loss: ',loss)
+                # print('loss_ST: ',loss_ST)
+                # print('loss_PS: ',loss_PS)
+                # print('loss_CDF: ',loss_CDF)
+                print('loss_L1: ',loss_L1)
+                print('loss_L2: ',loss_L2)
+                print('loss_mean: ',loss_mean)
+                # print('loss_bound: ',loss_bound)
+
+                # print(target_PS, PS)
+                # print(target_ST, ST)
 
             optimizer.zero_grad();
             loss.backward();
             optimizer.step();
 
-    np.save("../delta_recovery.npy", model_fit.param.cpu().detach().numpy());
 
-#---------------------------------------------------------------------------------------------------------
-if __name__ == '__main__':
-    generate_image()
+#----------------------------------------------------------------------------------------------------------
+    # save map
+    np.save(savedir +'synthesis_results_final.npy', model_fit.param.reshape(1,num_pixel,num_pixel).cpu().detach().numpy()-5);
+
+
+directory = '/home/yting'
+
+# lensing map
+# my_file = directory + 'image_initial.npy'
+# image = np.load(my_file)[0,:256,:256]
+
+# Delta function
+image = np.random.rand(256,256)<0.01
+
+# synthesise
+image_synthesis(image,
+                learnable_param_list = [(100*2, 1e-0),(100*50, 1e-1)],
+                savedir = directory)
