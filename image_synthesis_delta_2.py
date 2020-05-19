595e4fe893405b8213e83facc954ea6b39a939c1
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-05-19 18:42:56 -0400

    Update

--- image_synthesis_delta_2.py
+++ image_synthesis_delta_2.py
@@ -13,61 +13,6 @@ import numpy as np
 
 
 #=========================================================================================================
-def get_random_data(target, M, N, mode='image'):
-    '''
-    get a gaussian random field with the same power spectrum as the image 'target' (in the 'image' mode),
-    or with an assigned power spectrum function 'target' (in the 'func' mode).
-    '''
-
-    if mode == 'func':
-        random_phase = np.random.normal(0,1,(M//2-1,N-1)) + np.random.normal(0,1,(M//2-1,N-1))*1j
-        random_phase_left = (np.random.normal(0,1,(M//2-1)) + np.random.normal(0,1,(M//2-1))*1j)[:,None]
-        random_phase_top = (np.random.normal(0,1,(N//2-1)) + np.random.normal(0,1,(N//2-1))*1j)[None,:]
-        random_phase_middle = (np.random.normal(0,1,(N//2-1)) + np.random.normal(0,1,(N//2-1))*1j)[None,:]
-        random_phase_corners = np.random.normal(0,1,3)
-    if mode == 'image':
-        random_phase = np.random.rand(M//2-1,N-1)
-        random_phase_left = np.random.rand(M//2-1)[:,None]
-        random_phase_top = np.random.rand(N//2-1)[None,:]
-        random_phase_middle = np.random.rand(N//2-1)[None,:]
-        random_phase_corners = np.random.randint(0,2,3)/2
-    gaussian_phase = np.concatenate((
-                      np.concatenate((random_phase_corners[1][None,None],
-                                      random_phase_left,
-                                      random_phase_corners[2][None,None],
-                                      -random_phase_left[::-1,:],
-                                    ),axis=0),
-                      np.concatenate((np.concatenate((random_phase_top,
-                                                      random_phase_corners[0][None,None],
-                                                      -random_phase_top[:,::-1],
-                                                    ),axis=1),
-                                      random_phase,
-                                      np.concatenate((random_phase_middle,
-                                                      np.array(0)[None,None],
-                                                      -random_phase_middle[:,::-1],
-                                                    ),axis=1),
-                                      -random_phase[::-1,::-1],
-                                    ),axis=0),
-                                    ),axis=1)
-
-
-    if mode == 'image':
-        gaussian_modulus = np.abs(np.fft.fftshift(np.fft.fft2(target)))
-        gaussian_field = np.fft.ifft2(np.fft.fftshift(gaussian_modulus*np.exp(1j*2*np.pi*gaussian_phase)))
-    if mode == 'func':
-        X = np.arange(0,M)
-        Y = np.arange(0,N)
-        Xgrid, Ygrid = np.meshgrid(X,Y)
-        R = ((Xgrid-M/2)**2+(Ygrid-N/2)**2)**0.5
-        gaussian_modulus = target(R)
-        gaussian_modulus[M//2, N//2] = 0
-        gaussian_field = np.fft.ifft2(np.fft.fftshift(gaussian_modulus*gaussian_phase))
-
-    data = np.fft.fftshift(np.real(gaussian_field))
-    return data
-
-
-#=========================================================================================================
 # main body of the script
 def image_synthesis(image,
                     learnable_param_list = [(100, 1e-3)],
@@ -81,14 +26,6 @@ def image_synthesis(image,
     class model_image(nn.Module):
         def __init__(self):
             super(model_image, self).__init__()
-
-            # initialize with GRF of same PS as target image
-            if True:
-                self.param = torch.nn.Parameter(
-                    torch.from_numpy(
-                        get_random_data(image, 256, 256).reshape(1,-1)
-                    ).type(torch.cuda.FloatTensor) + 5
-                )
 
             # initialize with white noise GRF
             if True:
