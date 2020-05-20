1d78456d09f01b25a49c3c54552e9f83642b65a9
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-05-20 19:48:17 -0400

    Update

--- image_synthesis_weak_lensing.py
+++ image_synthesis_weak_lensing.py
@@ -121,7 +121,7 @@ def generate_image():
 
             loss_st = ((target_coeff[1:]-scattering_coeff[1:])**2).sum(); # ignore the zeroth order (normalization)
             loss_mean = ((model_fit.param.mean() - image_initial.mean()))**2
-            loss_L2 = ((model_fit.param**2).mean()**0.5 - (image_initial**2).mean()**0.5)\
+            loss_L2 = (((model_fit.param**2).mean()**0.5 - (image_initial**2).mean()**0.5)\
                             / (image_initial**2).mean()**0.5)**2
             #loss_cdf = ((torch.sort(model_fit.param).values[0,:] - CDF_t)**2).sum()/5.
 
