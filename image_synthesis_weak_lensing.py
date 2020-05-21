5bdfb12a91ae08c5694e4d1752e26e8cc7c8cb71
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-05-21 17:42:03 -0400

    Update

--- image_synthesis_weak_lensing.py
+++ image_synthesis_weak_lensing.py
@@ -158,7 +158,7 @@ def generate_image():
                 print('L1 loss', loss_L1)
                 print('L2 loss', loss_L2)
                 #print('L3 loss', loss_L3)
-                #print(((model_diff_std**3).mean()), ((image_diff_std**3).mean()))
+                print(model_cull.abs().mean(), image_initial.abs().mean())
                 print(' ')
                 np.save("../max_order=2_temp.npy", model_cull.cpu().detach().numpy());
                 np.save("../max_order=2_scatter_coeff_temp.npy", scattering_coeff.cpu().detach().numpy());
